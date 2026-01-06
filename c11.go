ad2444a2f290d7ed9a91c128f1df26d7c05e66b6
DerHeino <chan.heinrich@protonmail.com>
2026-01-06 14:10:15 +0100

    day 11 finished

--- c11.go
+++ c11.go
@@ -10,7 +10,7 @@ Graph problem: Find all paths fron a to b
 */
 
 func runC11() {
-	deviceMap := readFile("c11_test.txt", parseDevices)
+	deviceMap := readFile("c11.txt", parseDevices)
 	//printAllDevices(deviceMap)
 
 	execC11(deviceMap)
@@ -55,8 +55,27 @@ func parseDevices(content []byte) map[string]*device_S {
 
 type device_S struct {
 	id      string
-	inputs  uint64
+	inputs  map[string]uint64
 	outputs []*device_S
+}
+
+func (device *device_S) initInputs() {
+	if device.inputs == nil {
+		device.inputs = make(map[string]uint64, 1)
+	}
+}
+
+func (device *device_S) updateInputs(other *device_S) {
+	device.initInputs()
+	device.inputs[other.id] = other.getInputs()
+}
+
+func (device *device_S) getInputs() (acc uint64) {
+	for _, val := range device.inputs {
+		acc += val
+	}
+
+	return
 }
 
 func (device *device_S) print(linebreak bool) {
@@ -104,69 +123,57 @@ func execC11Two(deviceMap map[string]*device_S) {
 	pathMap := map[string]uint64{}
 
 	startReplaceStrategy(deviceMap, "svr", "fft")
-	pathMap["svr->fft"] = deviceMap["fft"].inputs
+	pathMap["svr->fft"] = deviceMap["fft"].getInputs()
 
 	resetInputs(deviceMap)
 	startReplaceStrategy(deviceMap, "fft", "dac")
-	pathMap["fft->dac"] = deviceMap["dac"].inputs
+	pathMap["fft->dac"] = deviceMap["dac"].getInputs()
 
 	resetInputs(deviceMap)
 	startReplaceStrategy(deviceMap, "dac", "out")
-	pathMap["dac->out"] = deviceMap["out"].inputs
+	pathMap["dac->out"] = deviceMap["out"].getInputs()
 
 	resetInputs(deviceMap)
 
 	solution := pathMap["svr->fft"] * pathMap["fft->dac"] * pathMap["dac->out"]
 	// path via svr->dac->fft->out not needed as there is no path from dac->fft
-	fmt.Println("Solution 2:", solution, pathMap)
+	fmt.Println("Solution 2:", solution)
 }
 
 func startReplaceStrategy(deviceMap map[string]*device_S, start string, end string) {
-	replacementSlice := make([]string, 0, len(deviceMap[start].outputs))
+	replacements := make(map[string]bool)
+
 	for _, device := range deviceMap[start].outputs {
-		device.inputs = 1
-		replacementSlice = append(replacementSlice, device.id)
+		device.initInputs()
+		device.inputs[start] = 1
+		replacements[device.id] = true
 	}
 
-	recursiveReplaceStrategy(replacementSlice, deviceMap, end)
+	replaceStrategy(replacements, deviceMap, end)
 }
 
-func recursiveReplaceStrategy(replacements []string, deviceMap map[string]*device_S, end string) {
+func replaceStrategy(replacements map[string]bool, deviceMap map[string]*device_S, end string) {
 	if len(replacements) == 0 {
 		return
 	}
 
-	nextReplacements := make([]string, 0, 10)
+	nextReplacements := make(map[string]bool)
 
-	for _, key := range replacements {
+	for key := range replacements {
 		device := deviceMap[key]
 		for _, output := range device.outputs {
-			output.inputs = output.inputs + device.inputs
+			output.updateInputs(device)
 			if output.id != end {
-				nextReplacements = append(nextReplacements, output.id)
+				nextReplacements[output.id] = true
 			}
 		}
 	}
 
-	nextReplacements = filterDuplicates(nextReplacements)
-	recursiveReplaceStrategy(nextReplacements, deviceMap, end)
-}
-
-func filterDuplicates(slice []string) []string {
-	set := make(map[string]uint8, len(slice))
-	newSlice := make([]string, 0, len(set))
-	for _, val := range slice {
-		if _, ok := set[val]; !ok {
-			set[val] = 1
-			newSlice = append(newSlice, val)
-		}
-	}
-
-	return newSlice
+	replaceStrategy(nextReplacements, deviceMap, end)
 }
 
 func resetInputs(deviceMap map[string]*device_S) {
 	for key := range deviceMap {
-		deviceMap[key].inputs = 0
+		deviceMap[key].inputs = nil
 	}
 }
